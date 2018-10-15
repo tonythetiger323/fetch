4bc6cddcad5bd76f1af428d9051ba4559cb9df68
Tony Greeley <tony.greeley@me.com>
2018-10-15 10:19:14 -0500

    Tony added assets and assets/javascript folders with firebase.js file that includes code to initialize the firebase realtime database I setup for the project

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA5X6ZpK5EnX9kFoHiRJuToB7u7blQDMq4",
+    authDomain: "groupproject-fetch.firebaseapp.com",
+    databaseURL: "https://groupproject-fetch.firebaseio.com",
+    projectId: "groupproject-fetch",
+    storageBucket: "groupproject-fetch.appspot.com",
+    messagingSenderId: "823895897115"
+};
+firebase.initializeApp(config);
