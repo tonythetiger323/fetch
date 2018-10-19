64e12731a16a91adcd3937f74306dce28e76fb31
Hugo Villalobos <hvillalo@linkedin.biz>
2018-10-18 19:04:03 -0500

    got JS API files to link to an html page

--- API.js
+++ API.js
@@ -1,3 +1,29 @@
+var smallDog = [
+
+]
+
+// var queryURL = "http://api.petfinder.com/my.method?key=977bb0c4a15623aed66ba3e238ac26bc&arg1=foo";
+
+
+$.getJSON('http://api.petfinder.com/pet.getRandom?format=json&key=977bb0c4a15623aed66ba3e238ac26bc&callback=?')
+    .then(function (petApiData) {
+        alert('Data retrieved!');
+        console.log(petApiData);
+    })
+    .catch(function (err) {
+        alert('Error retrieving data!');
+        console.log(err);
+    });
+function petApiData(data) {
+    console.log(data);
+}
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(response);
+// });
+
 //keys for the APIS
 //Pet Finder API: 
 // API Key
