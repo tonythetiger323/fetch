aa462fc6d975bd6f9d6326a7c998652ced71782f
Hugo Villalobos <hvillalo@linkedin.biz>
2018-10-20 11:36:39 -0500

    pushing changes

--- API.js
+++ API.js
@@ -11,18 +11,23 @@
 // API Status
 // Active
 
-var apiKey = '977bb0c4a15623aed66ba3e238ac26bc'; // assign our key to a variable, easier to read
+//Food API Key: l7xxc75c28da1f164896adcd222d338aa71c
 
+
+var apiKey = '977bb0c4a15623aed66ba3e238ac26bc'; // assign our key to a variable, easier to read
+var foodApiKey = "l7xxc75c28da1f164896adcd222d338aa71c"
 // the next line and function set up the button in our html to be clickable and reactive 
-document.addEventListener('DOMContentLoaded', bindButtons);
+$("button").on("click", bindButtons());
 
 function bindButtons() {
     document.getElementById('submitZip').addEventListener('click', function (event) {
         event.preventDefault();
         var zip = document.getElementById('zip').value; // this line gets the zip code from the form entry
         var url = 'http://api.petfinder.com/pet.getRandom';
+        var foodURL = 'https://api.shop.com:8443/AffiliatePublisherNetwork/v1/products?publisherID=TEST&locale=en_US&perPage=10&term=dog food'
 
         // Within $.ajax{...} is where we fill out our query 
+
         $.ajax({
             url: url,
             jsonp: "callback",
@@ -58,6 +63,42 @@ function bindButtons() {
                 list.appendChild(newImg);
             }
         });
-    })
+
+
+    });
 
 }
+
+var foodUrl = 'https://api.shop.com:8443/AffiliatePublisherNetwork/v1/products';
+var queryParams = '?' + encodeURIComponent('publisherID') + '=' + encodeURIComponent('TEST') + '&' + encodeURIComponent('locale') + '=' + encodeURIComponent('en_US') + '&' + encodeURIComponent('perPage') + '=' + encodeURIComponent('15') + '&' + encodeURIComponent('apikey') + '=' + encodeURIComponent('l7xxc75c28da1f164896adcd222d338aa71c');
+
+var queryURL = foodUrl + queryParams;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+
+    console.log(response);
+});
+
+$("#test").on("click", function () {
+
+    // Storing our giphy API URL for a random cat image
+
+    // Perfoming an AJAX GET request to our queryURL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+        // After the data from the AJAX request comes back
+        .then(function (response) {
+
+            console.log(response);
+        });
+});
+
+
+
+// GET https://api.shop.com:8443/AffiliatePublisherNetwork/v1/products?publisherID=TEST&locale=en_US&perPage=10&term=dog food
